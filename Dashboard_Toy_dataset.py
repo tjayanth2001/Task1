11196990dfcac5e5c0e1c550c9f78dfb5fe2b788
tjayanth2001 <93400194+tjayanth2001@users.noreply.github.com>
2023-02-26 22:55:01 +0530

    Add files via upload

---
+++ Dashboard_Toy_dataset.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from matplotlib import image
+import pandas as pd
+import plotly.express as px
+import os
+
+
+FILE_DIR=os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR=os.path.join(FILE_DIR,os.pardir)
+dir_of_interest=os.path.join(PARENT_DIR, "proj_2")
+IMAGE_PATH=os.path.join(dir_of_interest,"image","newyork.jpg")
+DATA_PATH=os.path.join(dir_of_interest,"data","toy_dataset.csv")
+
+st.title("Dashboard-Toydataset")
+
+img=image.open(IMAGE_PATH)
+st.image(img)
+
+df=pd.read_csv(DATA_PATH)
+st.dataframe(df)
+
+city=st.selectbox("select the city",df['City'].unique())
+col1, col2 = st.columns(2)
+fig_1 = px.histogram(df[df['City'] == city], x="Gender")
+col1.plotly_chart(fig_1, use_container_width=True)
+
+fig_2 = px.box(df[df['City'] == city],x='Gender',y="Income")
+col2.plotly_chart(fig_2, use_container_width=True)
